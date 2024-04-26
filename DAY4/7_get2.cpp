f962f85019170825f145ac8fdf83a487170bc164
ecourse <smkang@codenuri.co.kr>
2024-04-26 14:58:21 +0900

    Add files via upload

--- DAY4/7_get2.cpp
+++ DAY4/7_get2.cpp
@@ -26,9 +26,19 @@ int main()
 //	tuple<     double, short> t2;	// double 보관
 	tuple<int, double, short> t3(1, 3.4, 5); // 1, int 보관
 
-	std::cout << t3.value << std::endl; // ???
+	std::cout << t3.value << std::endl; // 1
 
-	std::cout << ? << std::endl; //  3.4
+	std::cout << static_cast<tuple<double, short>&>(t3).value << std::endl; //  3.4
 
-	std::cout << ? << std::endl; //  5
+	std::cout << static_cast<tuple<short>&>(t3).value << std::endl; //  5
+
+	auto n = get<0>(t3);
+	get<0>(t3) =10;
+}
+
+template<int N, typename TP>
+튜플_TP_가 가진 N 번째 요소의 타입& 
+get( TP& tp )
+{
+	return static_cast<TP의_N번째_부모_타입&>(tp).value;
 }
